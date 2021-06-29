2a0cde038b1c739f0aa41b660d93f92c02233b50
Kwon Hyeok Jin <lexky@naver.com>
2021-06-29 17:34:59 +0900

    feat:auth 미들웨어 추가로 인증체계 구현

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require("../models/User");
+const { auth } = require('../middleware/auth')
 
 //=================================
 //             User
 //=================================
+
+router.get("/auth", auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        image: req.user.image,
+    });
+});
 
 router.post("/register", (req, res) => {
 
@@ -24,12 +35,12 @@ router.post("/login", (req, res) => {
         if (!user)
             return res.json({
                 loginSuccess: false,
-                message: "Auth failed, email not found"
+                message: "이메일을 찾을 수 없습니다."
             });
 
         user.comparePassword(req.body.password, (err, isMatch) => {
             if (!isMatch)
-                return res.json({ loginSuccess: false, message: "Wrong password" });
+                return res.json({ loginSuccess: false, message: "비밀번호가 일치하지 않습니다." });
 
             user.generateToken((err, user) => {
                 if (err) return res.status(400).send(err);
@@ -38,14 +49,14 @@ router.post("/login", (req, res) => {
                     .cookie("w_auth", user.token)
                     .status(200)
                     .json({
-                        loginSuccess: true, userId: user._id
+                        isAuth : true, loginSuccess: true, userId: user._id, email: user.email, name: user.name
                     });
             });
         });
     });
 });
 
-router.get("/logout", (req, res) => {
+router.get("/logout", auth, (req, res) => {
     User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).send({
