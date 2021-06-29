f22ffe87b4f1319080b1dfac2bede323354fc0de
Kwon Hyeok Jin <lexky@naver.com>
2021-06-29 17:56:47 +0900

    fix: 기존에 Sidebar와 엘리먼트가 겹치지 않게 textAlign:'center'으로 처리하던 것 padding: "80px"으로 변경
    기존에 textAlign:'center'으로 처리할시 렌더링 폭이 커지면 똑같이 겹치는 현상이 일어나서
    Sidebar의 width만큼 padding을 주어 처리 또한  로그인 인증이 필요한 페이와 필요하지 않는페이지를(로그인, 회원가입)  Sidebar렌더링 처리

--- client/src/layouts/App/index.tsx
+++ client/src/layouts/App/index.tsx
@@ -7,16 +7,16 @@ const LogIn = loadable(() => import("../../pages/LogIn"));
 const SignUp = loadable(() => import("../../pages/SignUp"));
 const Calendar = loadable(() => import("../../pages/Calendar"));
 const Diary = loadable(() => import("../../pages/Diary"));
-const Mainpage = loadable(() => import("../../pages/Mainpage"))
+const Mainpage = loadable(() => import("../../pages/Mainpage"));
 
 const App = () => {
   return (
     <Switch>
-      <div style={{ textAlign:'center' }}>
-        <Sidebar />
+      <Route exact path="/login" component={LogIn}></Route>
+      <Route exact path="/signup" component={SignUp}></Route>
+      <Sidebar />
+      <div style={{ padding: "80px" }}>
         <Route exact path="/" component={Mainpage}></Route>
-        <Route exact path="/login" component={LogIn}></Route>
-        <Route exact path="/signup" component={SignUp}></Route>
         <Route exact path="/calendar" component={Calendar}></Route>
         <Route exact path="/diary" component={Diary}></Route>
       </div>
