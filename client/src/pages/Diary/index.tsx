bfd65a5b94e56706c5d959e9133d7135595d0a06
Kwon Hyeok Jin <lexky@naver.com>
2021-06-30 17:51:38 +0900

    feat: Diary 페이지 일기 등록 구현
    일기를 등록할 수 있는 페이지 구현하였다.
    날짜, 제목, 날씨, 내용으로 구성되었으며 각 컴포넌트의 스타일은 emotion으로 처리하였다.

--- client/src/pages/Diary/index.tsx
+++ client/src/pages/Diary/index.tsx
@@ -1,11 +1,94 @@
-import React from 'react'
+import React, { useCallback, useState } from "react";
+import locale from "antd/es/date-picker/locale/ko_KR";
+import { BiSend } from "react-icons/bi";
+import moment from "moment";
+import "moment/locale/ko";
+import {
+  Content,
+  Lightning,
+  Rain,
+  SelectedDatePicker,
+  SelectWheater,
+  Snow,
+  SubmitButton,
+  Sun,
+  Title,
+} from "./styles";
+import axios from "axios";
 
 const Diary = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [date, setDate] = useState(moment());
+  const [title, setTitle] = useState("");
+  const [wheather, setWheather] = useState("");
+  const [contents, setContents] = useState("");
 
-export default Diary
+  const titleChangeHandler = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      setTitle(e.currentTarget.value);
+    },
+    [setTitle]
+  );
+
+  const wheaterClickHandler = useCallback(
+    (wheather: string) => {
+      setWheather(wheather);
+    },
+    [setWheather]
+  );
+
+  const contentsChangeHandler = useCallback(
+    (e) => {
+      setContents(e.currentTarget.value);
+    },
+    [setContents]
+  );
+
+  const onsubmitHandler = () => {
+    const body = {
+      date: date,
+      title: title,
+      wheather: wheather,
+      contents: contents,
+    };
+
+    axios
+      .post("/api/diary/diary", body)
+      .then(() => {
+        
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <form onSubmitCapture={onsubmitHandler}>
+      <SelectedDatePicker
+        locale={locale}
+        format="LL dddd"
+        defaultValue={date}
+        disabled
+      />
+
+      <Title placeholder="제목" onChange={titleChangeHandler} value={title} />
+
+      <SelectWheater>
+        <Sun id="sun" onClick={() => wheaterClickHandler("sun")} />
+        <Rain id="rain" onClick={() => wheaterClickHandler("rain")} />
+        <Snow id="snow" onClick={() => wheaterClickHandler("snow")} />
+        <Lightning
+          id="lightning"
+          onClick={() => wheaterClickHandler("lightning")}
+        />
+      </SelectWheater>
+
+      <Content onChange={contentsChangeHandler} autoSize={{ minRows: 13 }} />
+
+      <SubmitButton type="primary" icon={<BiSend />} onSubmit={onsubmitHandler}>
+        작성
+      </SubmitButton>
+    </form>
+  );
+};
+
+export default Diary;
--- client/src/pages/Diary/styles.tsx
+++ client/src/pages/Diary/styles.tsx
@@ -1,9 +1,54 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
+import { Button, DatePicker, Input } from "antd";
+import {
+  BiCloudLightning,
+  BiCloudRain,
+  BiCloudSnow,
+  BiSun,
+} from "react-icons/bi";
+const { TextArea } = Input;
 
-export const Header = styled.header`
-  
+export const SelectWheater = styled.div`
+  font-size: 40px;
+  border: 1px solid black;
+
+  & > svg {
+    margin-right: 15px;
+  }
 `;
 
-export const Container = styled.div`
-  
+export const SubmitButton = styled(Button)`
+  margin-top: 1.25rem;
+  float: right;
+`;
+
+export const SelectedDatePicker = styled(DatePicker)`
+  width: 100%;
+  font-size: 30px;
+`;
+
+export const Title = styled(Input)`
+  width: 100%;
+  font-size: 22px;
+  font-weight: bold;
+`;
+
+export const Content = styled(TextArea)`
+  font-size: 18px;
+`;
+
+export const Rain = styled(BiCloudRain)`
+  color: #4169E1;
+`;
+
+export const Sun = styled(BiSun)`
+  color: #FF0000;
+`;
+
+export const Lightning = styled(BiCloudLightning)`
+  color: #AEB404;
+`;
+
+export const Snow = styled(BiCloudSnow)`
+  color: skyblue;
 `;
