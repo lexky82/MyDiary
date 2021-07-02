af47fdd9ff1418945089c54c3646d15cb628e23c
Kwon Hyeok Jin <lexky@naver.com>
2021-07-02 17:41:06 +0900

    feat: 구글맵, 이미지 업로더 컴포넌트 추가 및 각 컴포넌트 이벤트 추가
    구글맵, 이미지 업로더 컴포넌트에서 state를 변경하는 이벤트들은 부모 컴포넌트인 Diary 페이지에 추가하여 관리

--- client/src/pages/Diary/index.tsx
+++ client/src/pages/Diary/index.tsx
@@ -1,10 +1,15 @@
 import React, { useCallback, useState } from "react";
+
+/* Lib */
+import axios from "axios";
 import locale from "antd/es/date-picker/locale/ko_KR";
-import { BiSend } from "react-icons/bi";
 import moment from "moment";
-import Googlemap from "../../components/Googlemap";
-import ImageUploader from "react-images-upload";
 import "moment/locale/ko";
+
+/* Components */
+import { BiSend } from "react-icons/bi";
+import Googlemap from "../../components/Googlemap";
+import ImageUploader from "../../components/ImageUploader"
 import {
   Cloud,
   Content,
@@ -14,65 +19,73 @@ import {
   Rain,
   Sad,
   SelectedDatePicker,
-  SelectWheater,
+  SelectToday,
   Snow,
   SubmitButton,
   Sun,
   Title,
   Unhappy,
 } from "./styles";
-import axios from "axios";
-import { Button, Input, Modal } from "antd";
 
 const Diary = () => {
-  const [date, setDate] = useState(moment());
+  const [date] = useState(moment());
   const [title, setTitle] = useState("");
   const [wheather, setWheather] = useState("");
   const [contents, setContents] = useState("");
-  const [lng, setLng] = useState('');
-  const [image, setImage] = useState([]);
-  const [isSelected, setIsSelected] = useState(false);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-
+  const [mapLocation, setMapLocation] = useState({});
+  const [images, setImages] = useState<Array<{ key: number; value: object }>>(
+    []
+  );
   const titleChangeHandler = useCallback(
     (e: React.FormEvent<HTMLInputElement>) => {
       setTitle(e.currentTarget.value);
     },
-    [setTitle]
+    []
   );
 
-  const wheaterClickHandler = useCallback(
-    (wheather: string) => {
-      setWheather(wheather);
+  const wheaterClickHandler = useCallback((wheather: string) => {
+    setWheather(wheather);
+  }, []);
+
+  const contentsChangeHandler = useCallback((e) => {
+    setContents(e.currentTarget.value);
+  }, []);
+
+  const googleMapLngChangeHandler = useCallback(
+    (location: google.maps.MapMouseEvent) => {
+      const latLng = {
+        lat: location.latLng?.lat(),
+        lng: location.latLng?.lng(),
+      };
+      console.log(latLng);
+      setMapLocation({ ...latLng });
     },
-    [setWheather]
+    []
   );
 
-  const contentsChangeHandler = useCallback(
-    (e) => {
-      setContents(e.currentTarget.value);
-    },
-    [setContents]
-  );
+  const imgUploaderChangeHandler = (image: any) => {
+
+    setImages({ ...image });
+    console.log(images);
+  };
+
+  const deleteHandler = (image: any) => {
+    const currentIndex = images.indexOf(image);
+    console.log(currentIndex);
+
+    const newImages = [...images];
+    newImages.splice(currentIndex, 1);
+    setImages(newImages);
+  };
 
   const onsubmitHandler = () => {
     const body = {
       date: date,
       title: title,
       wheather: wheather,
+      location: mapLocation,
       contents: contents,
+      image: images,
     };
 
     axios
@@ -94,7 +107,7 @@ const Diary = () => {
 
       <Title placeholder="제목" onChange={titleChangeHandler} value={title} />
 
-      <SelectWheater>
+      <SelectToday>
         <Sun id="sun" onClick={() => wheaterClickHandler("sun")} />
         <Cloud onClick={() => wheaterClickHandler("cloud")} />
         <Rain id="rain" onClick={() => wheaterClickHandler("rain")} />
@@ -103,31 +116,37 @@ const Diary = () => {
           id="lightning"
           onClick={() => wheaterClickHandler("lightning")}
         />
-      </SelectWheater>
+      </SelectToday>
 
-      <SelectWheater>
+      <SelectToday>
         <Happy />
         <Normal />
         <Unhappy />
         <Sad />
-      </SelectWheater>
+      </SelectToday>
 
-        <Input disabled style={{ width : "30%" }} />
+      <Content
+        onChange={contentsChangeHandler}
+        autoSize={{ minRows: 15 }}
+        placeholder="오늘의 이야기를 써볼까요?"
+      />
 
-        <Button type="primary" onClick={showModal} >지도</Button>
-        <Modal
-          title="Map"
-          visible={isModalVisible}
-          onOk={handleOk}
-          onCancel={handleCancel}
-        >
-          <Googlemap />
-        </Modal>
+      <Googlemap
+        mapLngHandler={googleMapLngChangeHandler}
+        mapLocation={mapLocation}
+      />
 
-      <Content onChange={contentsChangeHandler} autoSize={{ minRows: 13 }} placeholder="오늘의 이야기를 써볼까요?" />
-
-      <ImageUploader withIcon={true} />
-
+      <div style={{ display:'flex'}}>
+        <ImageUploader imgUploaderChangeHandler={imgUploaderChangeHandler} />
+        <div>
+          {
+            images.map((image, index) => (
+              <img src="" alt="" key={index}/>
+            ))
+          }
+        </div>
+      </div>
+      
       <SubmitButton type="primary" icon={<BiSend />} onSubmit={onsubmitHandler}>
         작성
       </SubmitButton>
