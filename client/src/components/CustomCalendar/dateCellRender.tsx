d4b39e0420e38e5aed776fa54d015089cfaa3630
Kwon Hyeok Jin <lexky@naver.com>
2021-07-09 17:43:31 +0900

    feat: 자신이 작성한 일기의 Weather, Emotion을 달력에 표시 구현
    자신이 작성한 일기에 각 날에 맞는 Weather와 Emotion이 달력에 출력 됨.
    출력되는 Weather와 Emotion은 각각 Diary 페이지에서 사용했던 스타일 컴포넌트와 동일

--- client/src/components/CustomCalendar/dateCellRender.tsx
+++ client/src/components/CustomCalendar/dateCellRender.tsx
@@ -2,39 +2,88 @@ import React from "react";
 import moment from "moment";
 import useSWR from "swr";
 import fetcher from "../../utils/fetcher";
+import {
+  Cloud,
+  Lightning,
+  Rain,
+  Snow,
+  Sun,
+} from "../../utils/styles/weather_styledIcon";
+import {
+  Happy,
+  Normal,
+  Sad,
+  Unhappy,
+} from "../../utils/styles/emotion_styledIcon";
 
-const DateCellRender = () => {
-  const { data: diaryData } = useSWR("/api/diary/", fetcher);
+const DateCellRender = (value: moment.Moment) => {
+  const { data } = useSWR("/api/diary/", fetcher);
 
-  const getDiaryData = () => {
-    let diaryCellData;
+  const getDayInfo = () => {
+    const diaryData = data && data.diaryData;
+    let calendarData;
+    let diaryMoment;
 
-    switch (diaryData.createdAt.toDate()) {
-      case 1:
-        diaryCellData = [
+    for (let i in diaryData) {
+      diaryMoment = moment(diaryData[i].createdAt).format("YYYY-MM-DD");
+
+      if (diaryMoment === value.format("YYYY-MM-DD")) {
+        calendarData = [
           {
-            type: "warning",
-            content: diaryData.contents,
+            weather: diaryData[i].weather,
+            emotion: diaryData[i].emotion,
+            content: diaryData[i].content,
           },
         ];
-        break;
-
-      default:
-        break;
+      }
     }
 
-    return diaryCellData || [];
+    return calendarData || [];
   };
 
-  const listData = getDiaryData();
+  const WeatherRander = ({ weather }: { weather: string }) => {
+    switch (weather) {
+      case "sun":
+        return <Sun weather="none" style={{ fontSize: "50px" }} />;
+      case "cloud":
+        return <Cloud weather="none" style={{ fontSize: "50px" }} />;
+      case "rain":
+        return <Rain weather="none" style={{ fontSize: "50px" }} />;
+      case "snow":
+        return <Snow weather="none" style={{ fontSize: "50px" }} />;
+      case "lightning":
+        return <Lightning weather="none" style={{ fontSize: "50px" }} />;
+      default:
+        return <div></div>;
+    }
+  };
+
+  const EmotioinRender = ({ emotion }: { emotion: string }) => {
+    switch (emotion) {
+      case "happy":
+        return <Happy emotion="none" style={{ fontSize: "50px" }} />;
+      case "normal":
+        return <Normal emotion="none" style={{ fontSize: "50px" }} />;
+      case "unhappy":
+        return <Unhappy emotion="none" style={{ fontSize: "50px" }} />;
+      case "Sad":
+        return <Sad emotion="none" style={{ fontSize: "50px" }} />;
+      default:
+        return <div></div>;
+    }
+  };
+
+  const dayInfo = getDayInfo();
 
   return (
-    <ul>
-      {listData.map((item) => (
-        <li key={item.content}>
-        </li>
+    <div>
+      {dayInfo.map((info) => (
+        <div>
+          <WeatherRander weather={info.weather} />
+          <EmotioinRender emotion={info.emotion} />
+        </div>
       ))}
-    </ul>
+    </div>
   );
 };
 
