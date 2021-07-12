5364600ef261c6542c26cbfa99496a614a026c9b
Kwon Hyeok Jin <lexky@naver.com>
2021-07-12 17:38:24 +0900

    feat: Calendar 컴포넌트에서 날짜 선택시 해당 날짜에 작성한 일기 Modal 렌더링

--- client/src/components/CustomCalendar/index.tsx
+++ client/src/components/CustomCalendar/index.tsx
@@ -1,19 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
 /* Lib */
-import { Calendar } from "antd";
+import { Button, Calendar, Modal } from "antd";
 import locale from "antd/es/calendar/locale/ko_KR";
 
 /* Components */
 import dateCellRender from "./dateCellRender";
-import onSelectDateCell from "./onSelectDateCell"
+import moment from "moment";
+import useSWR from "swr";
+import fetcher from "../../utils/fetcher";
 
-const CustomCalendar = () => (
-  <Calendar
-    locale={locale}
-    dateCellRender={dateCellRender}
-    onSelect={onSelectDateCell}
-  />
-);
+type diaryType = {
+  id: string;
+  writer: string;
+  title: string;
+  emotion: string;
+  weather: string;
+  contents: string;
+  createdAt: string;
+  updatedAt: string;
+  location: { lat: number; lng: number };
+};
+
+const CustomCalendar = () => {
+  const { data } = useSWR("/api/diary/", fetcher);
+  const [DateCellModal, setDateCellModal] = useState(false);
+  const [selectDiary, setSelectDiary] = useState<diaryType>();
+
+  const handleOk = () => {
+    setDateCellModal(true);
+  };
+
+  const handleCancel = () => {
+    setDateCellModal(false);
+  };
+
+  const DiaryModal = () => (
+    <Modal
+      visible={DateCellModal}
+      title={selectDiary?.title}
+      onOk={handleOk}
+      onCancel={handleCancel}
+      footer={[
+        <Button key="back" onClick={handleCancel}>
+          수정
+        </Button>,
+        <Button key="submit" type="primary" onClick={handleCancel}>
+          확인
+        </Button>,
+      ]}
+    >
+      {selectDiary?.contents}
+    </Modal>
+  );
+
+  const onSelectDateCell = (value: moment.Moment) => {
+    const diaryData: Array<diaryType> = data && data.diaryData;
+
+    const filterData = diaryData.filter((data: any) => {
+      return (
+        moment(data.createdAt).format("YYYY-MM-DD") === value.format("YYYY-MM-DD")
+      );
+    });
+
+    filterData[0] && setSelectDiary(filterData[0]);
+    filterData[0] && setDateCellModal(true);
+  };
+
+  return (
+    <div>
+      <Calendar
+        locale={locale}
+        dateCellRender={dateCellRender}
+        onSelect={onSelectDateCell}
+      />
+
+      <DiaryModal />
+    </div>
+  );
+};
 
 export default CustomCalendar;
