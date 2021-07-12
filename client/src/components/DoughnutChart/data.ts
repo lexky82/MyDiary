32fd48a3596b59b352804f74be8fa14240434a8a
Kwon Hyeok Jin <lexky@naver.com>
2021-07-12 17:39:38 +0900

    feat: 일기에서 선택한 weather, emotion을 바탕으로 Chart 렌더링

---
+++ client/src/components/DoughnutChart/data.ts
@@ -0,0 +1,34 @@
+const data = [
+    {
+      "id": "scala",
+      "label": "scala",
+      "value": 558,
+      "color": "hsl(309, 70%, 50%)"
+    },
+    {
+      "id": "lisp",
+      "label": "lisp",
+      "value": 422,
+      "color": "hsl(208, 70%, 50%)"
+    },
+    {
+      "id": "go",
+      "label": "go",
+      "value": 34,
+      "color": "hsl(71, 70%, 50%)"
+    },
+    {
+      "id": "elixir",
+      "label": "elixir",
+      "value": 159,
+      "color": "hsl(267, 70%, 50%)"
+    },
+    {
+      "id": "sass",
+      "label": "sass",
+      "value": 504,
+      "color": "hsl(238, 70%, 50%)"
+    }
+  ]
+
+export default data
--- client/src/pages/Calendar/index.tsx
+++ client/src/pages/Calendar/index.tsx
@@ -4,10 +4,12 @@ import React, { useEffect } from "react";
 
 /* Components */
 import CustomCalendar from "../../components/CustomCalendar";
+import DoughnutChart from "../../components/DoughnutChart";
 
 const Calendar = () => {
   return (
     <div>
+      <DoughnutChart />
       <CustomCalendar />
     </div>
   );
