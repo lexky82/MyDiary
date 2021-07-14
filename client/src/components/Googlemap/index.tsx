7783985451ac542f9651e3e38928dea8869c80c7
Kwon Hyeok Jin <lexky@naver.com>
2021-07-14 17:57:45 +0900

    feat: Googlemap 컴포넌트에서 Marker의 positon을 props로 받고 Array일시 mapview에서 사용하는 것이므로 map 함수로 처리

--- client/src/components/Googlemap/index.tsx
+++ client/src/components/Googlemap/index.tsx
@@ -1,40 +1,49 @@
-import React from 'react'
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, LoadScript, Marker, MarkerProps } from "@react-google-maps/api";
 
 const center = {
   lat: 37.54,
-  lng: 127.04
+  lng: 127.04,
 };
 
 type props = {
-  mapLngHandler : Function,
-  mapLocation : any,
-  containerStyle : object,
-}
+  mapClickHandler: Function;
+  mapLocation: any | Array<Object>;
+  containerStyle: object;
+};
 
-const Googlemap = ({ mapLngHandler, mapLocation, containerStyle }: props) => {
+const Googlemap = ({ mapClickHandler, mapLocation, containerStyle }: props) => {
+
+  const mapviewMarkerClickHandler = (e: google.maps.MapMouseEvent) => {
+    const latLng = {
+      lat : e.latLng?.lat(),
+      lnt : e.latLng?.lng()
+    }
+
+    console.log(latLng)
+  }
 
   return (
     <LoadScript
       googleMapsApiKey="AIzaSyBnBqc7xRJzchvJALTI6m5qberlS0AOJK0"
       language="korean"
-      
     >
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={10}
-        
-        onClick={e => mapLngHandler(e)}
+        zoom={4}
+        onClick={(e) => mapClickHandler(e)}
       >
-        <Marker
-          position={mapLocation}
-        />
-        
-        <></>
+        {!Array.isArray(mapLocation) ? (
+          <Marker position={mapLocation} />
+        ) : (
+          mapLocation.map((location: google.maps.LatLng, index: number) => (
+            <Marker position={location} key={index} onClick={mapviewMarkerClickHandler} />
+          ))
+        )}
       </GoogleMap>
     </LoadScript>
-  )
-}
+  );
+};
 
-export default React.memo(Googlemap)
+export default React.memo(Googlemap);
