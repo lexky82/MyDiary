e5c5be637f3bd8f528c610831d92d1fa501406c1
Kwon Hyeok Jin <lexky@naver.com>
2021-07-16 17:56:26 +0900

    feat: 차트를 렌더링하는 스타일 컴포넌트 추가

---
+++ client/src/pages/Mainpage/styles.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const RenderChart = styled.div`
+  height: 50vh;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+    height: 35vh;
+  }
+`;
