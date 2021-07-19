0a6fa6a2112944f5c68be371d3b0bd29f33f84ff
Kwon Hyeok Jin <lexky@naver.com>
2021-07-19 17:15:02 +0900

    fix: Carousel에 있는 dots의 색이 흰색이므로 렌더링 하는 사진이 흰색일시 겹쳐서 보이지 않아 빨간색으로 변경

--- client/src/components/ImageSlider/styles.tsx
+++ client/src/components/ImageSlider/styles.tsx
@@ -2,8 +2,18 @@ import styled from "@emotion/styled";
 import { Carousel, Image } from "antd";
 
 export const Slider = styled(Carousel)`
-  width: 100%;
-  height : 60vh;
+  > .slick-dots li button {
+    width: 6px;
+    height: 6px;
+    border-radius: 100%;
+  }
+  > .slick-dots li.slick-active button {
+    width: 7px;
+    height: 7px;
+    border-radius: 100%;
+    background: red;
+  }
+  height : 50vh;
   background-color : black;
 
   @media screen and (max-width: 768px) {
@@ -12,7 +22,7 @@ export const Slider = styled(Carousel)`
 `;
 
 export const SliserImage = styled(Image)`
-  height : 60vh;
+  height : 50vh;
   
   @media screen and (max-width: 768px) {
     height : 40vh;
