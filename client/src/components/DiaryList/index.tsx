5623e785119e857bda5465b92f7e74758e13cb13
Kwon Hyeok Jin <lexky@naver.com>
2021-07-19 17:13:42 +0900

    fix: List.Item으로 수정, 삭제 버튼을 각각 일기에서 기록한 날씨, 기분 이모티콘으로 변경

--- client/src/components/DiaryList/index.tsx
+++ client/src/components/DiaryList/index.tsx
@@ -1,9 +1,11 @@
-import { List } from "antd";
+import { Button, List } from "antd";
 import moment from "moment";
 import 'moment/locale/ko'
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { diaryType } from "../../type";
+import { Happy, Normal, Sad, Unhappy } from "../../utils/styles/emotion_styledIcon";
+import { Cloud, Lightning, Rain, Snow, Sun } from "../../utils/styles/weather_styledIcon";
 import { Thumbnail } from "./styles";
 
 type listType = {
@@ -12,6 +14,8 @@ type listType = {
   description: string;
   content: string;
   image: string;
+  weather : string,
+  emotion : string,
 };
 
 type props ={
@@ -36,10 +40,44 @@ const DiaryList = ({selectedLocationDiary}: props) => {
           description: moment(diary.createdAt).format("YYYY-MM-DD"),
           content: textLengthOverCut(diary.contents, 60, "..."),
           image: diary.image[0],
+          weather: diary.weather,
+          emotion : diary.emotion,
         });
       });
 
     return [...listData];
+  };
+
+  const WeatherRander = ({ weather }: { weather: string }) => {
+    switch (weather) {
+      case "sun":
+        return <Sun size="30px" weather="none" />;
+      case "cloud":
+        return <Cloud size="30px" weather="none" />;
+      case "rain":
+        return <Rain size="30px" weather="none" />;
+      case "snow":
+        return <Snow size="30px" weather="none" />;
+      case "lightning":
+        return <Lightning size="30px" weather="none" />;
+      default:
+        return <div></div>;
+    }
+  };
+
+  const EmotioinRender = ({ emotion }: { emotion: string }) => {
+    switch (emotion) {
+      case "happy":
+        return <Happy size="30px" emotion="none" />;
+      case "normal":
+        return <Normal size="30px" emotion="none" />;
+      case "unhappy":
+        return <Unhappy size="30px" emotion="none" />;
+      case "sad":
+        return <Sad size="30px" emotion="none" />;
+      default:
+        return <div></div>;
+    }
   };
 
   const textLengthOverCut = (text: string, len: number, lastTxt: string) => {
@@ -66,8 +104,8 @@ const DiaryList = ({selectedLocationDiary}: props) => {
         renderItem={(item) => (
           <List.Item
             actions={[
-              <Link to="list-loadmore-edit">수정</Link>,
-              <Link to="list-loadmore-more">삭제</Link>,
+              <WeatherRander weather={item.weather} />,
+              <EmotioinRender emotion={item.emotion} />
             ]}
             extra={item.image && <Thumbnail alt="thumbnail" src={item.image} />}
           >
